f1890e114b80b0b5e89b7d761c609c7f560697ad
Thomas Dubot <tdubot@dh-w186-149.onecert.fr>
2019-11-08 16:50:38 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# Scrabble assistant - dictionary tools to improve your Scrabble level
+
+Basic functions using basic Python libraries (collections, itertools) to cleverly parse a list of valid words (e.g. ODS7)
+
+The demonstation notebook (demo_scrabble_assistant) shows how to use the dictionary class with the following examples:
+* Generate a list of words, e.g. all the 3-letters words incl. 'z'
+* Check if a word is valid
+* Get anagrams from a set of letters (and anagrams with one supplementary letter)
+* Get all the subwords from a set of letters
+* Get cousin words from a word
+* Get suffix words from a word
+
+## Built With
+
+* Python 3.7
+
+## Authors
+
+* **Thomas Dubot** 
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
+
+
+
---
+++ dictionary.py
@@ -0,0 +1,99 @@
+from collections import defaultdict
+from itertools import permutations, combinations
+import string
+
+class Dictionary:
+    def __init__(self, file):
+
+        self.d = defaultdict(lambda: defaultdict(list))
+        with open(file, "r") as f:
+            for l in f.readlines():
+                wd = l.strip().lower()
+                self.d[len(wd)]["".join(sorted(wd))].append(wd)
+        self.alphabet = list(string.ascii_lowercase)
+
+    def wordlist(
+        self, nb_letters, letters
+    ):  # get list of all words with 'nb_letters' letters and including the letters 'letters'
+        l = []
+        for key in self.d[nb_letters].keys():
+            if all(letter in key for letter in letters):
+                l.extend(self.d[nb_letters][key])
+        return l
+
+    def anagram(self, wd):  # get all valid words formed by all the letters in 'wd'
+        letters = "".join(sorted(wd))
+        if letters not in self.d[len(wd)].keys():
+            return []
+        return self.d[len(wd)][letters]
+
+    def valid(self, wd):  # return bool
+        return wd in self.anagram(wd)
+
+    def all_words(
+        self, wd
+    ):  # return dict nb_letters - list of valid words with nb_letters <= len(wd)
+        m = {}
+        for i in range(2, len(wd)):
+            lst = []
+            for j in combinations(wd, i):
+                swd = "".join(sorted(j))
+                if swd in self.d[i].keys():
+                    lst.extend(self.d[i][swd])
+            if len(lst) > 0:
+                m[i] = lst
+        return m
+
+    def words(
+        self, wd, nb_letters
+    ):  # return list of valid words with nb_letters composed with letters from wd
+        m = self.all_words(wd)
+        if nb_letters not in m.keys():
+            return []
+        return m[nb_letters]
+
+    def cousin(self, wd):  # return list of cousins of word wd
+        cousins = []
+        # alphabet = list(string.ascii_lowercase)
+        for i in range(len(wd)):
+            for letter in self.alphabet:
+                c = wd[:i] + letter + wd[i + 1 :]
+                if (c != wd) and self.valid(c):
+                    cousins.append(c)
+        return cousins
+
+    def suffix(self, wd):  # return list of suffixes
+        suffixes = []
+        for key in self.d.keys():
+            if key > len(wd):
+                for k, v in self.d[key].items():
+                    if all(letter in k for letter in wd):
+                        for word in self.d[key][k]:
+                            if word[: len(wd)] == wd:
+                                suffixes.append(word)
+        return suffixes
+
+    def anagram_plus_one(
+        self, wd
+    ):  # return dict letter - new word with only one different letter
+        m = defaultdict(list)
+        anagrams = []
+        for letter in self.alphabet:
+            wds = self.anagram(wd + letter)
+            if len(wds) > 0:
+                m[letter].extend(wds)
+        return m
+
+    def plus_one_words(self, wd):  # get list of all words possible with one more letter
+        all_wds = []
+        return sum(self.anagram_plus_one(wd).values(), [])
+
+    def plus_one_letters(
+        self, wd
+    ):  # which letters lead to a word composed of all letters of wd + this letter
+        return sorted(list(self.anagram_plus_one(wd).keys()))
+
+    def plus_one_letter_words(
+        self, wd, letter
+    ):  # get list of all words possible composed of all letters of wd + the letter as input
+        return self.anagram_plus_one(wd)[letter]
