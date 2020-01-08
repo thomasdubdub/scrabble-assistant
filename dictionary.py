3aaa20c9db212debde2b1c8b404805e70e6be214
Thomas Dubot <tdubot@dh-w186-161.onecert.fr>
2020-01-08 11:35:21 +0100

    new functions to compare a dictionary with another

--- dictionary.py
+++ dictionary.py
@@ -2,18 +2,23 @@ from collections import defaultdict
 from itertools import permutations, combinations
 import string
 
+
 class Dictionary:
     def __init__(self, file):
-
-        self.d = defaultdict(lambda: defaultdict(list))
+        self.alphabet = list(string.ascii_lowercase)
+        self.l = []  # list of all words
+        self.d = defaultdict(
+            lambda: defaultdict(list)
+        )  # dictionary: key 1 = nb of letters key 2 = sorted letters
         with open(file, "r") as f:
             for l in f.readlines():
                 wd = l.strip().lower()
                 self.d[len(wd)]["".join(sorted(wd))].append(wd)
-        self.alphabet = list(string.ascii_lowercase)
+                self.l.append(wd)
+        self.l = sorted(self.l)
 
     def wordlist(
-        self, nb_letters, letters
+        self, nb_letters=2, letters=""
     ):  # get list of all words with 'nb_letters' letters and including the letters 'letters'
         l = []
         for key in self.d[nb_letters].keys():
@@ -54,7 +59,6 @@ class Dictionary:
 
     def cousin(self, wd):  # return list of cousins of word wd
         cousins = []
-        # alphabet = list(string.ascii_lowercase)
         for i in range(len(wd)):
             for letter in self.alphabet:
                 c = wd[:i] + letter + wd[i + 1 :]
@@ -97,3 +101,27 @@ class Dictionary:
         self, wd, letter
     ):  # get list of all words possible composed of all letters of wd + the letter as input
         return self.anagram_plus_one(wd)[letter]
+
+    def diff_suppr(
+        self, other
+    ):  # get all words that were in other and are not available in the current dictionary
+        return sorted(list(set(other.l) - set(self.l)))
+
+    def diff_add(
+        self, other, nb_letters=0, letters=""
+    ):  # get all new words from the other dictionary
+        l = []
+        if nb_letters > 1:
+            l = list(
+                set(self.wordlist(nb_letters, letters))
+                - set(other.wordlist(nb_letters, letters))
+            )
+        else:  # 0 means all words
+            for key in self.d.keys():
+                l.extend(
+                    list(
+                        set(self.wordlist(key, letters))
+                        - set(other.wordlist(key, letters))
+                    )
+                )
+        return sorted(l)
